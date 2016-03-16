5937f256f9da553976e55c043b38cf2b000d522c
nmvenuti <nmv7de@virginia.edu>
2016-03-16 14:17:16 -0400

    I'm scared :/

--- prototype/nmv_test_script.R
+++ prototype/nmv_test_script.R
@@ -15,7 +15,7 @@ library(RcppArmadillo)
 library(stringr)
 source("prototype/method_preprocessing.R")#Libraries in this package bring into main script
 
-source('prototype/method_quantiyContext.R')
+source('prototype/method_quantifyContext.R')
 
 
 ########################################
@@ -29,24 +29,24 @@ window_length=15
 most_freq_words=c('god','lord')
 sim_count=1000
 
-#Create processed_tokens fils
-dataCorpus <- VCorpus(DirSource(paste(filepath,"/Raw",sep="")))
-preprocessDocuments(dataCorpus,filepath,datafile_name)
-
-#Test output of cooccurrences
-
-createWordCoOccurences(filepath,datafile_name,processedTokens)
-# dsmProj<-dsmProjTest 
-
-#Test DSM function
-load(paste(filepath,"/Rdata/wordCooccurences_",datafile_name,'.Rdata',sep=""))
-wordCo<-wordCooccurences
-createDSM(filepath,datafile_name,wordCo)
-
-#Test cv function
-load(paste0(filepath, "/Rdata/dsmProj_", datafile_name, ".RData"))
-
-testCV<-quantifyContext(filepath,datafile_name,processedTokens,dsmProj,most_freq_words,minMatches=25,window_length=15,sim_count=1000)
+# #Create processed_tokens fils
+# dataCorpus <- VCorpus(DirSource(paste(filepath,"/Raw",sep="")))
+# preprocessDocuments(dataCorpus,filepath,datafile_name)
+# 
+# #Test output of cooccurrences
+# 
+# createWordCoOccurences(filepath,datafile_name,processedTokens)
+# # dsmProj<-dsmProjTest 
+# 
+# #Test DSM function
+# load(paste(filepath,"/Rdata/wordCooccurences_",datafile_name,'.Rdata',sep=""))
+# wordCo<-wordCooccurences
+# createDSM(filepath,datafile_name,wordCo)
+# 
+# #Test cv function
+# load(paste0(filepath, "/Rdata/dsmProj_", datafile_name, ".RData"))
+# 
+# testCV<-quantifyContext(filepath,datafile_name,processedTokens,dsmProj,most_freq_words,minMatches=25,window_length=15,sim_count=1000)
 
 # Create RData files containing Co-occurences of Each Random Group
 for (group in groups) {
@@ -89,17 +89,29 @@ for (group in groups) {
   print(group)
   for (type in c('train','test')){
     dataFiles <- list.files(paste0('./data_dsicap/',group,"/RData"))
-    bins <- dataFiles[str_detect(dataFiles,paste0(type,"[0-9]+"))]
+    bins <- dataFiles[str_detect(dataFiles,paste0('processedTokens_',type,"[0-9]+"))]
     for (binnedTokens in bins) {
       print(binnedTokens)
+      
+      #Load processed tokens
       load(paste0('./data_dsicap/',group,'/RData/',binnedTokens))
       
-      metrics <- quantifyContext(filepath,datafile_name,target_corpus,dsmProj,most_freq_words,minMatches=25,window_length=15,sim_count=1000)
+      #Load DSM
+      dsmName<-paste0('dsmProj_',gsub("processedTokens_", "", binnedTokens))
+      load(paste0('./data_dsicap/',group,'/RData/',dsmName))
+
+      #Load targetWords
+      targetName<-paste0('targetWords_',gsub("processedTokens_", "", binnedTokens))
+      load(paste0('./data_dsicap/',group,'/RData/',targetName))
+      
+      metrics <- quantifyContext(filepath,datafile_name,target_corpus,dsmProj,targetWords$Var1[1:20],minMatches=25,window_length=15,sim_count=1000)
       cvColumns<-paste('word_',1:length(most_freq_words),sep="")
       colnames(testCV)<-c('groupName',metrics)
       cvMetrics <- rbind(cvMetrics, metrics)
       
       remove(processedTokens)
+      remove(dsmProj)
+      remove(targetWords)
     }
   }
 }
