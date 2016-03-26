9fcc0f1fca05105a3e35eb007b423929ecdacb8b
Hope McIntyre <hope.e.mcintyre@gmail.com>
2016-03-26 11:07:39 -0400

    tracking bash script for 328 par

--- check_bash.sh
+++ check_bash.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+file='run_parSemCo'
+ext='.sh'
+path='./scripts/'
 for i in `seq 1 10`;
 do
-    echo $i
+    	
+	# s = "$filename$i$e"
+	# echo "$filename"
+	# echo "$e"
+	echo sbatch $path$file$i$ext
+	# echo "$s"
+
 done
