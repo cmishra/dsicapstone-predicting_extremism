aa7e688b68b96954500ba02b37ef2b687bbcc320
cmishra <cm5ky@virginia.edu>
2016-03-27 12:55:19 -0400

    undid LDA changes

--- prototype/method/method_run_LDA.R
+++ prototype/method/method_run_LDA.R
@@ -3,20 +3,13 @@ run_LDA <- function(traincorpus, k, groupids, docids, sparsity, testcorpus)
   
   dtm <- DocumentTermMatrix(traincorpus)
   dtm <- removeSparseTerms(dtm, sparse=sparsity)
-  require(slam)
-  num_words <- row_sums(dtm)
-  dtm <- dtm[num_words > 0,]
-  print(names(num_words)[num_words == 0])
-  
+
   topics <- LDA(dtm, k)
   topic_probabilities <- as.data.frame(topics@gamma)
   colnames(topic_probabilities) <- lapply(seq(k), function(x) { paste0("Topic_",x)})
   
   testdtm <- DocumentTermMatrix(testcorpus)
   testdtm <- removeSparseTerms(testdtm, sparse=sparsity)
-  num_words <- row_sums(testdtm)
-  testdtm <- testdtm[num_words > 0,]
-  print(names(num_words)[num_words == 0])
   preds <- posterior(topics, newdata = testdtm)
   colnames(preds$topics) <- lapply(seq(k), function(x) { paste0("Topic_",x)})
   
